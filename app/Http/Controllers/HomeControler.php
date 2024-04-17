059f664e6bfb9262bb4474477c2ca59535a5a187
Pablo Serey <pabloserey1995@hotmail.com>
2024-04-17 19:03:16 -0400

    invoke

--- app/Http/Controllers/HomeControler.php
+++ app/Http/Controllers/HomeControler.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeControler extends Controller
 {
-    //
+    public function __invoke(){
+        return "hola desde la pagina de inicio";
+    }
 }
+ /*controladores con un unico metodo mejor metodos invocable */
