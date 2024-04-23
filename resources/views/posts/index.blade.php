0bd28c8cddd7a0cf9e803190b4467bb52765b8e0
Pablo Serey <pabloserey1995@hotmail.com>
2024-04-22 22:23:53 -0400

    class

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,38 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Free</title>
+
+    <style>
+        .color{
+            color: red;
+        }
+
+        .color2{
+            color: blue;
+        }
+    </style>
+
 </head>
 <body>
     <h1>listado de Posts</h1>
 
-    <!-- loop -->
+    <!-- Class -->
     
     <ul>
-
         @foreach ($posts as $post)
-        <li>
+        <li @class([
+            "color" => $loop->first,
+            "color2" => $loop->last
+        ])>
             <h2>
                 {{ $post['title']}}
-
-                (Indice restantes: {{$loop->remaining}})
 
             </h2>
             <p>
                 {{ $post["content"]}}
             </p>
-            <ul>
-                @foreach ($post["tags"] as $tag)
-                    <li>{{ $tag }}
-                        
-                        @if ($loop->parent->first)
-                            (este es el primer tag del post)
-                        @endif
-                    </li>
-                @endforeach
-            </ul>
-
+        </li>
         @endforeach
         </li>
     </ul>
+
 </body>
 </html>
