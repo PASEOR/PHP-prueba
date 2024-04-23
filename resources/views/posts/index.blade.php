04f043bcc4f233128db0187015cb2d6adc82fa91
Pablo Serey <pabloserey1995@hotmail.com>
2024-04-22 22:39:23 -0400

    directiva include

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,39 +6,65 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Free</title>
 
-    <style>
-        .color{
-            color: red;
-        }
-
-        .color2{
-            color: blue;
-        }
-    </style>
-
 </head>
 <body>
     <h1>listado de Posts</h1>
 
-    <!-- Class -->
-    
-    <ul>
-        @foreach ($posts as $post)
-        <li @class([
-            "color" => $loop->first,
-            "color2" => $loop->last
-        ])>
-            <h2>
-                {{ $post['title']}}
+    <!-- especial -->
 
-            </h2>
-            <p>
-                {{ $post["content"]}}
-            </p>
-        </li>
-        @endforeach
-        </li>
-    </ul>
+    <form action="">
+
+        <div>
+            <label for="">
+                <input type="checkbox" name="paises[]" id= "">
+                peru
+            </label>
+            <label for="">
+                <input type="checkbox" @checked(true) name="paises[]" id= "">
+                chile
+            </label>
+            <label for="">
+                <input type="checkbox" name="paises[]" id= "">
+                argentina
+            </label>
+            <label for="">
+                <input type="checkbox" name="paises[]" id= "">
+                brasil
+            </label>
+
+        </div>
+
+        <div>
+            <select name="ciudad" >
+                <option value="1">
+                    lima
+                </option>
+                <option value="2">
+                    santiago    
+                </option>
+                <option value="3">
+                    buenos aires
+                </option>
+                <option value="4" @selected(true)>
+                    brasilia
+                </option>
+            </select>
+        </div>
+
+        <div>
+            <input type="text" @readonly(true)>
+        </div>
+
+        <div>
+            <input type="text" @required(true)>
+        </div>
+        <button @disabled(true)>
+            Enviar
+        </button>
+
+    </form>
+    
+    
 
 </body>
 </html>
