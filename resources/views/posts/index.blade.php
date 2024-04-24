aac10826c28bdbbabac9f3df3a2170685df089db
Pablo Serey <pabloserey1995@hotmail.com>
2024-04-24 19:05:40 -0400

    componentes como plantilla

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,32 +1,17 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Free</title>
-
-    <script src="https://cdn.tailwindcss.com"></script>
-
-</head>
-<body>
-    <!--  componentes anonimos solo componente de vista-->
+<x-app-layout>
+    <!--  componentes como plantilla-->
     <x-container width="7xl">
     
-        <x-alert class="mb-32" type="danger">   <!--atributos-->
+        <x-alert class="mb-32" type="info">  
 
-            <x-slot name="title"> {{--slot datos largos--}}
+            <x-slot name="title"> 
                 Titulo de prueba 
             </x-slot>
 
-            change a few things up and try submitting again. {{--slot principal--}}
+            change a few things up and try submitting again. 
         </x-alert>
 
-        <p>
-            hola mundos
-        </p>
     
     </x-container>
-</body>
 
-</html>
+</x-app-layout>
