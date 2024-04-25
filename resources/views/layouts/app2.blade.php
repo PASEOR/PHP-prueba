243ae7686394787754255c23debbf1992eff32ee
Pablo Serey <pabloserey1995@hotmail.com>
2024-04-24 23:33:03 -0400

    directiva stack

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -6,12 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
 
+    @stack('meta')
+
     <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body>
-    {{--herencia de plantilla no existen los slot - los slot son propios de los componentes
-    contenido variable -> puntos de insercion 
+    {{--directiva stack     
+        diferencia de stack y yield, stack permite apilar el contenido a traves de los push 
+        nota: la herencia es para laravel de7 para abajo conteiner es un componente de laravel 8 y superior  
     --}}
+   
 
     @yield('content')
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,6 +3,12 @@
 {{--modificar los valores con section--}}
 
 @section('title', "coders free")
+
+@push('meta')
+
+    <meta name="description" content="Aprende a programar desde cero">
+    
+@endpush
     
 @section("content")
     <x-container width="7xl">
@@ -16,7 +22,6 @@
             change a few things up and try submitting again. 
         </x-alert>
 
-    
     </x-container>
 @endsection
 
